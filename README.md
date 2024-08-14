a190365847ee37c295a0e7288ceef6d89f36ad38
Babita <das.tat96@gmail.com>
2024-08-14 19:33:52 +0530

    New codes are added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java Program
+- Core Java
+- OOps
+- Collection Framework
+- Exception Handing
+- 
---
+++ src/ex_14082024/Lab001.java
@@ -0,0 +1,10 @@
+package ex_14082024;
+
+public class Lab001 {
+    public static void main(String[] args)
+    //public static void main(String args[]) same
+
+        {
+        System.out.println("Hello World");
+    }
+}
