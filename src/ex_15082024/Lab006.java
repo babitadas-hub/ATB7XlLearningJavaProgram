a4a427d9e3194fca43d5bb60d9b05569cdd9ea3e
Babita <das.tat96@gmail.com>
2024-08-15 21:09:44 +0530

    New codes are added

--- src/ex_15082024/Lab006.java
+++ src/ex_15082024/Lab006.java
@@ -4,7 +4,7 @@ public class Lab006 {
     public static void main(String[] args) {
         byte b = 10;
         System.out.println(b);
-        //JVM- brother who always start executing from the main method
+        //JVM- who always start executing from the main method
         // 10- decimal
         // machine(0,1) ->00001010 - 8 bits
         long Distance_moon_earth = 3844000000000000L;
---
+++ src/ex_15082024/Lab007.java
@@ -0,0 +1,18 @@
+package ex_15082024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+            //for storing age what datatype we can use?
+        //Max age of a person in the world? 127 ?
+                byte age = 122;// age > 127 // lose memory -> smart container - good practice
+                int age1 = 100; // here we are wasting memory - 214768348488
+        // program store pi with only 2 decimal i.e. 3.14, but the real value is - 3.142323237866999
+        float pi = 3.14f; //smart container - good practice
+        double pi_d =  3.14876545678; // this is not required because our requirement is to store only 2
+        // decimal value
+
+        // byte - data type
+        // Byte -> memory allocation
+        // if we will click on power save mode then we can't see any error in intellij
+    }
+}
