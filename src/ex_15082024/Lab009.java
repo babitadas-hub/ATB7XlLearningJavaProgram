011b7e8425a243d4f63f50e1f48916c4dd063e39
Babita <das.tat96@gmail.com>
2024-08-16 01:17:33 +0530

    New source codes are added

---
+++ src/ex_15082024/Lab009.java
@@ -0,0 +1,24 @@
+package ex_15082024;
+
+public class Lab009 {
+
+        public static void main(String[] args) {
+            byte b = 127;
+            // b = b+1;
+            System.out.println(b); // +1; Size related issues will come
+
+            char grade = 'A';
+            char grade_kusum = 'E';
+            char grade_wswati = 'B';
+            char c = '!';
+            char c2 = '@';
+            char _123 = 'N';
+
+            // Can I change the data type of variable between program
+            // No
+            int age = 123;
+            // String age = "Suraj";
+            // it's not possible. Java won't allow to change data type in-between the program
+        }
+}
+
---
+++ src/ex_15082024/Lab013.java
@@ -0,0 +1,11 @@
+package ex_15082024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+            String name = "Pramod";
+            System.out.println(name); // O/P: Pramod
+            System.out.printf("Your name is -> %s",name); //O/P: Your name is -> Pramod
+        }
+    }
+
+
