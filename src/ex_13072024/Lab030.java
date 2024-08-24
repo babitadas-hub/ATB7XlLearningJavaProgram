2cc4c26db15be70c8e73082340869f735f1dab01
Babita <das.tat96@gmail.com>
2024-08-24 19:31:41 +0530

    New source codes are added

--- src/ex_13072024/Lab030.java
+++ src/ex_13072024/Lab030.java
@@ -1,12 +1,18 @@
 package ex_13072024;
 
 public class Lab030 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
-        boolean c = (10 >= 10); // here this condition is going to check 10 > 10 or 10 = 10
-        // Whenever we have "or" and any of the condition become true, then result will be true
-
-        System.out.println(c);
+        public static void main (String[]args)
+        {
+            System.out.println(10 == 10); // == Compare two values
+            System.out.println(10 >= 10); //  10> 10 or 10 = 10
+            System.out.println(10 <= 10); // 10 < 10 or 10 = 10
+            System.out.println(10 > 10);
+            System.out.println(10 < 10);
+        }
     }
-}
+/** O/P:
+ true
+ true
+ true
+ false
+ false **/
---
+++ src/ex_20072024/Lab090.java
@@ -0,0 +1,14 @@
+package ex_20072024;
+
+public class Lab090 {
+    public static void main(String[] args) {
+        char ch = 'y';
+        if (ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u')
+        {
+            System.out.println("vowel");
+        }
+        else{
+            System.out.println("consonant");
+        }
+    }
+}
